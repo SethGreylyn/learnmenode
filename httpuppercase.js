5e145b7db203ac9c757b15f6d94a2c6f81a6f392
sgreylyn <seth.greylyn@gmail.com>
2018-03-14 23:44:40 -0700

    Penultimate task

--- httpuppercase.js
+++ httpuppercase.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const http = require('http');
+const map = require('through2-map');
 const port = Number(process.argv[2]);
 
 const server = http.createServer((req, resp) => {
-
+    if (req.method !== 'POST') {
+        return resp.end('Could not handle non-POST request');
+    }
+    req.pipe(map(chunk => {
+        return chunk.toString().toUpperCase()
+    })).pipe(resp);
 });
 
 server.listen(port);
